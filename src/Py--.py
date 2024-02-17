c05b8ba7f5d525b7e96d4fcb12394ab416d6c7f8
1Codealot <58225804+1Codealot@users.noreply.github.com>
2024-02-17 09:02:53 +0000

    Initial commit; currently tokenises.

---
+++ src/Py--.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys 
+import Tokenisation.Tokeniser as Tokeniser
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: Py--.py <file>")
+        return
+    file = sys.argv[1]
+
+    with open(file, "r") as f:
+        code = f.read()
+
+        tokeniser:Tokeniser = Tokeniser.Tokeniser(code)
+
+        print(tokeniser.tokenise())
+        
+
+if __name__ == "__main__":
+    main()
---
+++ src/Tokenisation/Tokeniser.py
@@ -0,0 +1,39 @@
+class Tokeniser:
+    def __init__(self, text:str):
+        self.text:str = text
+
+        self.found_tokens:list[str] = []
+        self.tokens_to_find:list[str] = [
+                "(",
+                ")",
+                "!",
+                "\"",
+                "\n"
+                ]
+        
+        self.buf:str = ""
+        self.idx:int = 0
+        self.program_len:int = len(text)
+
+    def consume(self):
+        # print(f"consuming {self.buf}")
+
+        self.found_tokens.append(self.buf)
+
+        print(f"consumed: {self.found_tokens[-1]}")
+
+        self.buf = ""
+
+    def tokenise(self):
+        # Go through the code and look to find all the tokens
+        while self.idx < self.program_len:
+            self.buf += self.text[self.idx]
+            if self.idx == self.program_len - 1:
+                self.consume()
+            else:
+                if self.buf in self.tokens_to_find or self.text[self.idx+1] in self.tokens_to_find:
+                    self.consume()
+
+            self.idx += 1
+
+        return self.found_tokens
