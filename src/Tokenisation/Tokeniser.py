fcce5a889ce553eb6dcde122a28e841d0df2b06d
1Codealot <58225804+1Codealot@users.noreply.github.com>
2024-02-17 09:25:36 +0000

    Added better string parsing

--- src/Tokenisation/Tokeniser.py
+++ src/Tokenisation/Tokeniser.py
@@ -15,7 +15,24 @@ class Tokeniser:
         self.idx:int = 0
         self.program_len:int = len(text)
 
+    def parse_string(self):
+        # Find the other "
+        self.idx += 1
+
+        try:
+            while self.text[self.idx] != "\"":
+                self.buf += self.text[self.idx]
+                self.idx += 1
+
+            self.buf += self.text[self.idx]
+        except IndexError:
+            print("Error: No end of string found.")
+            exit(1)
+
     def consume(self):
+        if self.buf == "\"":
+            self.parse_string()
+
         self.found_tokens.append(self.buf)
         self.buf = ""
 
@@ -25,8 +42,8 @@ class Tokeniser:
             self.buf += self.text[self.idx]
             if self.idx == self.program_len - 1:
                 self.consume()
-            elif self.buf in self.tokens_to_find or self.text[self.idx+1] in self.tokens_to_find:
-                    self.consume()
+            elif self.buf in self.tokens_to_find or self.text[self.idx+1] in self.tokens_to_find: # language idea xor
+                self.consume()
 
             self.idx += 1
 
