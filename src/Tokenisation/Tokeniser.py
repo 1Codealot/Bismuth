0602dbdfd65d11e78eada7fe9979555b35c79215
1Codealot <58225804+1Codealot@users.noreply.github.com>
2024-03-20 18:45:09 +0000

    Added license comments to all src files

--- src/Tokenisation/Tokeniser.py
+++ src/Tokenisation/Tokeniser.py
@@ -1,3 +1,8 @@
+# This project, and all source code, is licensed under the BSD 3-Clause license.
+# This should be found at the root of this project's directory.
+# If you cannot find it, you can find a copy here: https://github.com/1Codealot/Bismuth/blob/master/LICENSE
+
+
 tokens_to_find: list[str] = [
     # Symbols
     "(",
